bb4d6fc5ba531f78b10c3aa8ecc10e3b0b07cb7f
baotran1311 <baotran13112003@gmail.com>
2025-02-18 22:01:41 +0700

    Feat/Tran:Add Alert Success Add To Cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { router } from "./config/router";
 import { RouterProvider } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer} from 'react-toastify';
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <ToastContainer />
     </>
   );
 }
