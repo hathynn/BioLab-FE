683f7c1bc4bd43c0b216b7a866a5aa3574297f1c
hathynn <jennyhatyn@gmail.com>
2025-02-18 16:01:21 +0700

    Fix/Nhu: Payment UIUI

--- src/components/templates/create-blog/index.tsx
+++ src/components/templates/create-blog/index.tsx
@@ -95,7 +95,7 @@ function CreateBlog() {
       />
 
       <button
-        onClick={() => console.log(content)}
+        onClick={() => console.log(value)}
         className="mt-4 px-4 py-2 border hover:bg-customGreen hover:text-white text-gray-500 font-semibold text-base rounded-lg"
       >
         Đăng bài
