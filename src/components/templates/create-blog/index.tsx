65eae4bf6d5e8f3d45d9e56b098a9316c24dba98
hathynn <jennyhatyn@gmail.com>
2025-02-18 16:00:58 +0700

    Fix/Nhu: fix error payment ui

--- src/components/templates/create-blog/index.tsx
+++ src/components/templates/create-blog/index.tsx
@@ -1,11 +1,11 @@
 import { useState, useRef } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import axios from "axios";
+
 import "./index.scss";
 
 function CreateBlog() {
-  const [content, setContent] = useState<string>("");
+
   const [value, setValue] = useState("");
   const reactQuillRef = useRef<ReactQuill>(null);
 
