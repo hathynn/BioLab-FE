0542cc897a65d99ad1ad10db6a4d60ea6191c942
Nguyen Ngoc Y Nhi <ynhiworkplace308@gmail.com>
2025-03-26 05:45:19 +0700

    Feat/Nhi: Update Total Payment Bill

--- src/config/openAI.tsx
+++ src/config/openAI.tsx
@@ -1,4 +1,6 @@
 import OpenAI from "openai";
+import useProductService from "../services/useProductService";
+import { ProductType } from "../types/product.type";
 
 const openai = new OpenAI({
   apiKey:
@@ -8,11 +10,19 @@ const openai = new OpenAI({
 
 export async function sendMsgToOpenAI(
   message: string,
+  products: ProductType[],
   onChunkReceived: (chunk: string) => void
 ) {
   const stream = await openai.chat.completions.create({
     model: "gpt-4o-mini",
-    messages: [{ role: "user", content: message }],
+    messages: [
+      {
+        role: "system",
+        content: `Bạn là một chuyên gia tư vấn dược phẩm. Dựa trên danh sách thuốc bên dưới, hãy giúp người dùng tìm sản phẩm phù hợp với triệu chứng của họ. 
+      Danh sách thuốc: ${JSON.stringify(products)}`,
+      },
+      { role: "user", content: message },
+    ],
     store: true,
     stream: true,
   });
