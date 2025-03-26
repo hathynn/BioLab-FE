b703d8d0dc73337b4d00bd0d9909d06a83e2ee8a
baotran1311 <baotran13112003@gmail.com>
2025-03-26 16:43:51 +0700

    Fix/Tran:Fix UI Create Product

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -247,7 +247,7 @@ function HomePage() {
                   </button>
                 </div>
                 <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-10">
-                  {products?.map((product, index) => (
+                  {products?.slice(0, 4)?.map((product, index) => (
                     <RecommendationProduct
                       key={index}
                       _id={product._id}
